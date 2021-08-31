913072915fce77ef2af991f28beef724db307c71
Daniel <danmoran.pro@gmail.com>
2021-08-31 13:33:50 -0600

    refractored. assignment completed

--- authenticator.rb
+++ authenticator.rb
@@ -1,9 +1,7 @@
-require 'pry'
-
   user_1 = {username: "danmoran", password: "password1"}
   user_2 = {username: "alexmoran", password: "password2"}
   user_3 = {username: "saulmoran", password: "password3"}
-  users = [user_1, user_2, user_3]
+  @users = [user_1, user_2, user_3]
 
   puts " ~~~*** Welcome to Authenticator ***~~~ "
    75.times { print "-"}
@@ -13,23 +11,25 @@ require 'pry'
   
   attempts = 0
   
-  while attempts < 4 
+  def authenticate(username, password)
+    @users.each do |user|
+      if user[:username] == username && user[:password] == password
+        return user
+      end
+    end
+    "Invalid credentials"
+  end 
+
+  while attempts < 3
     print "Username: "
     username = gets.chomp 
     print "Password: "
     password = gets.chomp
-    
-    users.each do |user|
-      if user[:username] == username && user[:password] == password
-        puts user
-        break
-      else
-        puts "Invalid credentials"
-      end
-    end 
-
+    authentication = authenticate(username, password)
+    puts authentication
     puts "Press 'n' or any other key to continue: "
     input = gets.chomp.downcase
     break if input == "n"
     attempts += 1
   end 
+  puts "you have exceeded the number of attempts" if attempts == 3
